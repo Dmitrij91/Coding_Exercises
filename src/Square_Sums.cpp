a96ac2706189ba640c5e46c1c3b36c41f6560583
Martin B. <uajew2@gmail.com>
2024-11-03 11:53:47 +0100

    Determining neighbors corrected

--- src/Square_Sums.cpp
+++ src/Square_Sums.cpp
@@ -3,37 +3,46 @@
 #include <math.h>
 #include <iostream>
 
+void printNeighbours(std::vector<std::set<int>> neighbours)
+{
+  std::vector<std::set<int>>::iterator it;
+  int index = 1;
+  for(auto it = neighbours.begin();it!=neighbours.end();it++)
+  {
+    std::set<int>::iterator it_in;
+    std::cout << index << " : [";
+    for(auto it_in = (*it).begin();it_in!=(*it).end();it_in++)
+    {
+      std::cout << *it_in << ",";
+    }
+    std::cout << "]\n";
+    index++;
+  }
+}
+
 void square_sums_row(int n)
 {
   std::vector<std::set<int>> neighbours;
   for(int i = 1;i<=n;i++)
   {   
-      int root = floor(sqrt(i))+1;
-      int square = pow(root,2);
+      int root = floor(sqrt(i));
+      int square = pow(root+1,2);
       int first_neighbour = square-i;
       std::set<int> aux;
-      for(int j = first_neighbour;j< n;j += 2*root+1)
+      for(int j = first_neighbour; j <= n; j += 2*root + 1)
       {
-        aux.emplace(j);
+        if(i != j)
+          aux.emplace(j);
         root++;
       }
       neighbours.push_back(aux);
   }
-  std::vector<std::set<int>>::iterator it;
-  for(auto it = neighbours.begin();it!=neighbours.end();it++)
-  {
-    std::set<int>::iterator it_in;
-    for(auto it_in = (*it).begin();it_in!=(*it).end();it_in++)
-    {
-      std::cout << *it_in << ",";
-    }
-    std::cout << "\n";
-  }
+  printNeighbours(neighbours);
 }
 
 int main()
 {
-  int input = 10;
+  int input = 100;
   square_sums_row(input);
   return 0;
 }
